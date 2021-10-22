feb9b3cf1882b45e8135dc63a815807dfaa8c2f9
rongkezan <kezan.r@renrenbit.com>
2021-10-22 10:31:22 +0800

    train

--- demo.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-from PIL import ImageFilter
-
-img = Image.open("dataset/raw/raw_crop/front/baf84ca6-28d9-11ec-8c55-acde48001122-4.jpg")
-imgF = img.filter(ImageFilter.CONTOUR)
-imgF.show()
--- train.py
+++ train.py
@@ -1,25 +1,12 @@
 import os
-import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-import numpy as np
 import torch
 from torch import nn
 import torch.optim as optim
-import torchvision
-from torchvision import transforms, models, datasets
-import imageio
+from torchvision import transforms, models
 import time
-import warnings
-import random
-import sys
 import copy
-import json
-from PIL import Image
-
-import util
-import model
-from train_model import train_model
 
 data_dir = './dataset/'
 train_dir = data_dir + '/train'
@@ -32,6 +19,7 @@ data_transforms = {
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 均值，标准差
     ]),
     'valid': transforms.Compose([
+        transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -48,118 +36,113 @@ class_names = image_datasets['train'].classes
 # util.show_img(dataloaders['valid'], class_names)
 
 # 是否用GPU训练
-train_on_gpu = torch.cuda.is_available()
-
-if not train_on_gpu:
-    print('CUDA is not available.  Training on CPU ...')
-else:
-    print('CUDA is available!  Training on GPU ...')
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model_ft, input_size = model.initialize_model('resnet', 2, feature_extract=False, use_pretrained=False)
+# 采用 Densenet 模型
+model = models.densenet161()
+model.classifier = nn.Linear(model.classifier.in_features, 2)
+print(model)
 
 params_to_update = []
-for name, param in model_ft.named_parameters():
+for name, param in model.named_parameters():
     if param.requires_grad:
         params_to_update.append(param)
         print("\t", name)
 
-# 优化器设置
-optimizer_ft = optim.Adam(params_to_update, lr=1e-5)
-scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)  # 学习率每7个epoch衰减成原来的1/10
-# 最后一层已经LogSoftmax()了，所以不能nn.CrossEntropyLoss()来计算了，nn.CrossEntropyLoss()相当于logSoftmax()和nn.NLLLoss()整合
-criterion = nn.NLLLoss()
-
-model_ft, val_acc_history, train_acc_history, valid_losses, train_losses, LRs = \
-    train_model(model_ft, dataloaders, criterion, optimizer_ft, 'checkpoint.pth', device, scheduler, num_epochs=20)
+# 优化器
+optimizer = optim.Adam(params_to_update, lr=1e-2)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)  # 学习率每7个epoch衰减成原来的1/10
+criterion = nn.CrossEntropyLoss()
 
 
+def do_train(filename, num_epochs=20):
+    """
+    checkpoint = torch.load(filename)
+    best_acc = checkpoint['best_acc']
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    model.class_to_idx = checkpoint['mapping']
+    """
+    since = time.time()
+    best_acc = 0
+    val_acc_history = []
+    train_acc_history = []
+    train_losses = []
+    valid_losses = []
+    LRs = [optimizer.param_groups[0]['lr']]
+
+    model.to(device)
+    best_model_wts = copy.deepcopy(model.state_dict())
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # 训练和验证
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # 把数据都取个遍
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # 清零
+                optimizer.zero_grad()
+                # 只有训练的时候计算和更新梯度
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    _, preds = torch.max(outputs, 1)
+                    # 训练阶段更新权重
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # 计算损失
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            time_elapsed = time.time() - since
+            print('Time elapsed {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # 得到最好那次的模型
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                state = {
+                    'state_dict': model.state_dict(),
+                    'best_acc': best_acc,
+                    'optimizer': optimizer.state_dict(),
+                }
+                torch.save(state, filename)
+            if phase == 'valid':
+                val_acc_history.append(epoch_acc)
+                valid_losses.append(epoch_loss)
+                scheduler.step(epoch_loss)
+            if phase == 'train':
+                train_acc_history.append(epoch_acc)
+                train_losses.append(epoch_loss)
+
+        print('Optimizer learning rate : {:.7f}'.format(optimizer.param_groups[0]['lr']))
+        LRs.append(optimizer.param_groups[0]['lr'])
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # 训练完后用最好的一次当做模型最终的结果
+    model.load_state_dict(best_model_wts)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 利用DataLoader加载数据集
-# data_loaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in
-#                 ['train', 'valid']}
-# train_data_loader = DataLoader(train_data, batch_size=64)
-# test_data_loader = DataLoader(test_data, batch_size=64)
-
-# # 创建网络模型
-# net = model.Net()
-#
-# # 创建损失函数
-# loss_fn = nn.CrossEntropyLoss()
-#
-# # 优化器
-# optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
-#
-# # 设置训练网络的一些参数
-# # 记录训练次数
-# total_train_step = 0
-# # 记录测试的次数
-# total_test_step = 0
-# # 记录训练的轮数
-# epoch = 10
-# # 添加Tensor Board
-# writer = SummaryWriter("logs")
-#
-# for i in range(epoch):
-#     print("--- 第 {} 轮训练开始 ---".format(i + 1))
-#     # 训练步骤开始
-#     total_train_loss = 0
-#     total_train_accuracy = 0
-#     for data in train_data_loader:
-#         imgs, target = data
-#         output = net(imgs)
-#         loss = loss_fn(output, target)
-#         total_train_loss += loss.item()
-#         accuracy = (output.argmax(1) == target).sum().item()
-#         total_train_accuracy += accuracy
-#
-#         # 优化器优化模型
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#         total_train_step += 1
-#         if total_train_step % 10 == 0:
-#             print("训练次数: {}, Loss: {}".format(total_train_step, loss.item()))
-#             writer.add_scalar("train_loss", loss.item(), total_train_step)
-#
-#     print("整体训练集上的loss: {}".format(total_train_loss))
-#     print("整体训练集上的准确率:{}".format(total_train_accuracy / train_data_size))
-#     writer.add_scalar("train_loss", total_train_loss, total_test_step)
-#     writer.add_scalar("train_accuracy", total_train_accuracy / train_data_size, total_test_step)
-#
-#     # 测试步骤开始
-#     total_test_loss = 0
-#     total_test_accuracy = 0
-#     with torch.no_grad():
-#         for data in test_data_loader:
-#             imgs, target = data
-#             output = net(imgs)
-#             loss = loss_fn(output, target)
-#             total_test_loss += loss.item()
-#             accuracy = (output.argmax(1) == target).sum().item()
-#             total_test_accuracy += accuracy
-#     print("整体测试集上的loss: {}".format(total_test_loss))
-#     print("整体测试集上的准确率:{}".format(total_test_accuracy / test_data_size))
-#     writer.add_scalar("test_loss", total_test_loss, total_test_step)
-#     writer.add_scalar("test_accuracy", total_test_accuracy / test_data_size, total_test_step)
-#     total_test_step += 1
-#     torch.save(net, "model/net_{}.pth".format(i))
-#
-# writer.close()
+do_train('checkpoint.pth')
