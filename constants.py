75064a93acb67d33f3be31bbceaa7b9f575c1f46
戎柯赞 <764058263@qq.com>
2021-10-24 23:54:44 +0800

    1

--- constants.py
+++ constants.py
@@ -1,67 +1,19 @@
 import os
 
 # 截图存档目录
-img_dir_path = r'images'
-flag_dir_path = r'images/flag'
-sub_dir_path = r'images/sub'
-data_dir_path = r'images/data'
+flag_dir = "img/flag/"
+data_dir = "img/data/"
+temp_dir = "img/temp/"
 
-model_dir_path = r'model'
-
-driver_dir_path = r'driver'
-##############
-
-# images
-img_sc_path = os.path.join(img_dir_path, 'mhxy.jpg')
-
-img_desktop_name = 'desktop.jpg'
-img_desktop_path = os.path.join(img_dir_path, img_desktop_name)
-
-# images/data/
-train_dir = os.path.abspath(data_dir_path + '/train')
-validation_dir = data_dir_path + '/validation'
-new_dir_path = os.path.abspath(data_dir_path + '/new')
-
-front_img_dir = os.path.join(train_dir, 'front')
-others_img_dir = os.path.join(train_dir, 'others')
-
-new_front_img_dir = os.path.abspath(new_dir_path + '/front')
-new_others_img_dir = os.path.abspath(new_dir_path + '/others')
-
-# model/
-model_path = os.path.join(model_dir_path, 'mhxy.h5')
-
-# images/sub
-fight_shape = (793, 182, 805, 436)
-fighting_img_path = os.path.join(sub_dir_path, 'fighting.jpg')
-
-popup_sub_img_path = os.path.join(sub_dir_path, 'pop_sub.jpg')
-
-crop_4_img_names = ['1.jpg', '2.jpg', '3.jpg', '4.jpg']
-crop_4_img_paths = [os.path.join(sub_dir_path, crop_4_img_name) for crop_4_img_name in crop_4_img_names]
-
-# images/flag
-fighting_flag_img_path = os.path.join(flag_dir_path, 'fighting_flag.jpg')
-
-popup_flag_img_paths = [os.path.join(flag_dir_path, popup_flag_img_name)
-                        for popup_flag_img_name in ['popup_flag_1.jpg', 'popup_flag_2.jpg']]
-
-mouse_flag_img_name = 'mouse_flag.jpg'
-mouse_flag_img_path = os.path.join(flag_dir_path, mouse_flag_img_name)
-
-# driver
-driver_name = 'kmclass'
-
-kmclass_dll_path = os.path.abspath(driver_dir_path + '/kmclassdll.dll').replace('\\', '\\\\')
-
-kmclass_driver_path = os.path.abspath(driver_dir_path + '/kmclass.sys').replace('\\', '\\\\')
-
-# 截图大小
+sc_img = os.path.join(temp_dir, 'temp_game.jpg')
+desktop_img = os.path.join(temp_dir, 'temp_desktop.jpg')
+popup_img = os.path.join(temp_dir, 'popup_sub.jpg')
+fight_img = os.path.join(flag_dir, 'fight_flag.jpg')
+fighting_flag_img_path = os.path.join(flag_dir, 'fight_flag.jpg')
+popup_flag_img = os.path.join(flag_dir, 'popup_flag.jpg')
+mouse_flag_img = os.path.join(flag_dir, 'mouse_flag.jpg')
+fight_shape = (795, 179, 805, 438)  # 战斗截图Shape
 screen_size = (812, 663)
-sub_size = (360, 120)
-
-# 移动偏移 弹框偏移
-popup_move_shapes = [(-84, 33, 96, 140), (-82, 29, 182, 135)]
-
-# 鼠标偏移
-mouse_move_shape = (16, 15)
+sub_size = (360, 134)  # 四小人大小
+popup_move_shape = (-56, 16, 118, 130)  # 弹框偏移
+mouse_move_shape = (16, 15)  # 鼠标偏移
--- snap.py
+++ snap.py
@@ -1,67 +1,55 @@
-# import win32gui
+import win32gui
 import util
 import sys
 import os
+import shutil
 import constants as c
 from PIL import Image
 from skimage.metrics import structural_similarity
 import cv2 as cv
 from PyQt5.QtWidgets import QApplication
+import time
 
 hwnd_title = dict()
 
 
-# def get_all_hwnd(hwnd, mouse):
-#     if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
-#         hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
-
-
-def fight_crop():
-    util.log_title('战斗标识截图')
-    return crop(c.img_sc_path, c.fighting_img_path, c.fight_shape)
+def get_all_hwnd(hwnd, mouse):
+    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
 
 
 def popup_crop():
     util.log_title('弹窗判断')
-    shape_dict = {}
-    for i in range(len(c.popup_flag_img_paths)):
-        shape, score = template_match(c.popup_flag_img_paths[i], c.img_sc_path)
-        shape_dict[shape] = (score, i)
-
-    print(shape_dict)
-    max_shape = max(shape_dict, key=shape_dict.get)
-    score, i = shape_dict[max_shape]
-    print(f'最大区域 {max_shape} 最终得分为 {score}')
+    shape, score = template_match(c.popup_flag_img, c.sc_img)
+    print(f'弹框提示区域 {shape} 最终得分为 {score}')
     if score >= 3:
         sub_shape = (
-            max_shape[0] + c.popup_move_shapes[i][0],
-            max_shape[1] + c.popup_move_shapes[i][1],
-            max_shape[2] + c.popup_move_shapes[i][2],
-            max_shape[3] + c.popup_move_shapes[i][3]
+            shape[0] + c.popup_move_shape[0],
+            shape[1] + c.popup_move_shape[1],
+            shape[2] + c.popup_move_shape[2],
+            shape[3] + c.popup_move_shape[3]
         )
         print(f'弹框区域  {sub_shape}')
-        return crop(c.img_sc_path, c.popup_sub_img_path, sub_shape)
+        img_path = os.path.join(c.temp_dir, "popup_data.jpg")
+        crop(c.sc_img, img_path, sub_shape)
+        return img_path
     print(f'没有弹框')
-    return False
 
 
 def crop(source_path, target_path, shape):
     with Image.open(source_path) as img:
-        fighting_flag_img = img.crop(shape)
-        fighting_flag_img.save(target_path)
+        img.crop(shape).save(target_path)
         return True
 
 
 def template_match(template_path, src_path):
     img = cv.imread(src_path, 0)
-    img2 = img.copy()
     template = cv.imread(template_path, 0)
     w, h = template.shape[::-1]
     methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
                'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
     shape_dict = {}
     for meth in methods:
-        img = img2.copy()
         method = eval(meth)
         # Apply template Matching
         res = cv.matchTemplate(img, template, method)
@@ -80,28 +68,30 @@ def template_match(template_path, src_path):
     return max_shape, shape_dict[max_shape]
 
 
-# def shot():
-#     util.log_title('截图')
-#     win32gui.EnumWindows(get_all_hwnd, 0)
-#     title = ''
-#     for h, t in hwnd_title.items():
-#         if t.startswith('梦幻西游 ONLINE'):
-#             title = t
-#             print(title)
-#             hwnd = win32gui.FindWindow(None, title)
-#             app = QApplication(sys.argv)
-#             desktop_id = app.desktop().winId()
-#             screen = QApplication.primaryScreen()
-#             img_desk = screen.grabWindow(desktop_id).toImage()
-#             img_sc = screen.grabWindow(hwnd).toImage()
-#             img_desk.save(c.img_desktop_path)
-#             img_sc.save(c.img_sc_path)
-#             print(f'img_desktop save to -> {os.path.abspath(c.img_desktop_path)}')
-#             print(f'img_mhxy save to -> {os.path.abspath(c.img_sc_path)}')
-#     if title == '':
-#         print('mhxy not start')
-#         return False
-#     return True
+def shot():
+    util.log_title('截图')
+    win32gui.EnumWindows(get_all_hwnd, 0)
+    title = ''
+    for h, t in hwnd_title.items():
+        if t.startswith('梦幻西游 ONLINE'):
+            title = t
+            print(title)
+            hwnd = win32gui.FindWindow(None, title)
+            app = QApplication(sys.argv)
+            desktop_id = app.desktop().winId()
+            screen = QApplication.primaryScreen()
+            img_desk = screen.grabWindow(desktop_id).toImage()
+            img_sc = screen.grabWindow(hwnd).toImage()
+            img_desk.save(c.desktop_img)
+            img_sc.save(c.sc_img)
+            print(f'img_desktop save to -> {os.path.abspath(c.desktop_img)}')
+            print(f'img_mhxy save to -> {os.path.abspath(c.sc_img)}')
+    if title == '':
+        print('mhxy not start')
+        return False
+    if image_check(c.sc_img, c.screen_size):
+        return False
+    return True
 
 
 def image_check(img_path, size):
@@ -115,13 +105,15 @@ def image_check(img_path, size):
 
 
 def is_fight():
-    util.log_title('状态判断')
-    rate = compare_image(c.fighting_flag_img_path, c.fighting_img_path)
+    util.log_title('战斗状态判断')
+    crop(c.sc_img, c.fight_img, c.fight_shape) # 战斗标识截图
+    rate = compare_image(c.fighting_flag_img_path, c.fight_img)
+    print(rate)
     if rate > 0.95:
-        print('战斗 状态')
+        print('战斗状态')
         return True
     else:
-        print('非战斗 状态')
+        print('非战斗状态')
         return False
 
 
@@ -138,23 +130,45 @@ def crop_4():
     util.log_title('弹窗人物切分')
     w = 90
     h = 120
-    for i in range(len(c.crop_4_img_names)):
+    for i in range(4):
         shape = (w * i, 0, w * (i + 1), h)
-        crop(c.popup_sub_img_path, c.crop_4_img_paths[i], shape)
+        img_name = str((i + 1)) + str(int(round(time.time() * 1000))) + ".jpg"
+        crop(c.popup_img, os.path.join(c.data_dir, img_name), shape)
 
 
-def locate(screen, wanted, show=0):
-    loc_pos = []
+def is_same_fight(img):
+    if img is None:
+        return True
+    # 得到最新的一张保存的四小人截图
+    files = os.listdir(c.data_dir)
+    recent_file = None
+    for file in files:
+        if recent_file is None:
+            recent_file = file
+        elif convert_to_int(file) > convert_to_int(recent_file):
+            recent_file = file
+    # 确认刚截的图不是已经保存过的
+    shape, score = template_match(recent_file, img)
+    if score >= 3:
+        return False
+    return True
+
+
+def convert_to_int(file_name):
+    return int(str(file_name).split('.')[0])
+
+
+def task():
+    if shot() & is_fight():
+        temp_img = popup_crop()
+        if is_same_fight(temp_img):
+            return
+        else:
+            shutil.copy(os.path.join(c.temp_dir, "popup_data.jpg"),
+                        os.path.join(c.data_dir, str(int(round(time.time() * 1000))) + ".jpg"))
 
 
 if __name__ == '__main__':
-    shape, score = template_match(r'images/flag/popup_flag_1.jpg', r'images/flag/1.png')
-    print(shape, score)
-    # if shot():  # 截图
-    #     if image_check(c.img_sc_path, c.screen_size):  # 检查截图大小
-    #         fight_crop()  # 战斗标识截图
-    #         if is_fight():  # 判断是否在战斗
-    #             if popup_sub_crop():  # 弹窗识别 与 人物区域切出
-    #                 if image_check(c.popup_sub_img_path, c.sub_size):  # 弹窗人物截图检查
-    #                     crop_4()  # 弹窗人物切分
-    #                     print()
+    while True:
+        time.sleep(3)
+        task()
