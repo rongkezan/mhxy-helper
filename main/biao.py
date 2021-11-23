bb6b602eed3ae6fc8cd19ed5b5fef27ea8b54f3a
rongkezan <kezan.r@renrenbit.com>
2021-11-23 19:41:22 +0800

    跑镖优化

--- main/biao.py
+++ main/biao.py
@@ -142,11 +142,11 @@ def release_cargo(npc_name):
             b_move(x, y)
 
 
-def transfer_yz():
+def yz_transfer():
     """
-    驿站车夫传送
+    驿站车夫传送，如果识别当前地点为大唐国境则退出循环，否则一直找车夫
     """
-    while True:
+    while not is_place("大唐国境"):
         time.sleep(0.1)
         meet_robber()
         info("寻找驿站车夫")
@@ -160,80 +160,91 @@ def transfer_yz():
             b_move(x, y)
             info("驿站车夫传送大唐国境")
             b_move(268, 472)
-            if is_place("大唐国境"):
-                break
         else:
-            warn("未能找到驿站车夫")
+            warn("未能找到驿站车夫，继续寻找")
 
 
 def ca_to_dtjw():
     """
     去大唐境外
     """
-    b_move_map("cac", 0, 0, log="长安 -> 驿站")
-    transfer_yz()
-    b_move_map("dtgj", 0, 0, log="大唐国境 -> 大唐境外")
-    b_move(45, 237, log="进入大唐境外")
+    b_move_map("cac", 283, 38, log="长安 -> 驿站")
+    yz_transfer()
+    while not is_place("大唐境外"):
+        b_move_map("dtgj", 5, 84, log="大唐国境 -> 大唐境外")
+        b_move(45, 237, log="进入大唐境外")
 
 
 def ca_to_dhw():
     """
     去东海湾
     """
-    b_move_map("cac", 0, 0, log="长安 -> 江南野外")
-    b_move(0, 0, log="进入江南野外")
-    b_move_map("jnyw", 0, 0, log="江南野外 -> 建邺城")
-    b_move(0, 0, log="进入建邺城")
-    b_move_map("jyc", 0, 0, log="建邺城 -> 东海湾")
-    b_move(0, 0, log="进入东海湾")
+    while not is_place("江南野外"):
+        b_move_map("cac", 545, 10, log="长安 -> 江南野外")
+        b_move(0, 0, log="进入江南野外")
+    while not is_place("建邺城"):
+        b_move_map("jnyw", 135, 56, log="江南野外 -> 建邺城")
+        b_move(0, 0, log="进入建邺城")
+    while not is_place("东海湾"):
+        b_move_map("jyc", 274, 32, log="建邺城 -> 东海湾")
+        b_move(0, 0, log="进入东海湾")
 
 
 def qf():
     b_move_map("cac", 88, 79, log="长安 -> 秦府")
     b_move(343, 260, log="进入秦府")
-    if is_place("秦府"):
+    while not is_place("秦府"):
         b_move(196, 209, 5, log="寻找秦琼")
         release_cargo("秦琼")
 
 
 def dt():
-    b_move_map("cac", 0, 0, log="长安 -> 大唐官府")
-    b_move(343, 86, log="进入大唐官府")
-    if is_place("大唐官府"):
-        b_move_map("dtgf", 0, 0, log="大唐官府 -> 大唐官府府邸")
+    while not is_place("大唐官府"):
+        b_move_map("cac", 312, 277, log="长安 -> 大唐官府")
+        b_move(343, 86, log="进入大唐官府")
+    while not is_place("大唐官府府邸"):
+        b_move_map("dtgf", 74, 48, log="大唐官府 -> 大唐官府府邸")
         b_move(339, 310, log="进入大唐官府府邸")
-        if is_place("大唐官府府邸"):
-            b_move(363, 334, 5, log="寻找程咬金")
-            release_cargo("程咬金")
+    while not is_place("大唐官府府邸"):
+        b_move(363, 334, 5, log="寻找程咬金")
+        release_cargo("程咬金")
 
 
 def hs():
-    b_move_map("cac", 0, 0, log="长安 -> 化生寺")
-    b_move(497, 106, log="进入化生寺")
-    b_move_map("hss", 0, 0, log="化生寺 -> 化生寺庙")
-    b_move(482, 293, log="进入化生寺庙")
+    while not is_place("化生寺"):
+        b_move_map("cac", 513, 277, log="长安 -> 化生寺")
+        b_move(513, 277, log="进入化生寺")
+    while not is_place("化生寺庙"):
+        b_move_map("hss", 92, 56, log="化生寺 -> 化生寺庙")
+        b_move(482, 293, log="进入化生寺庙")
     release_cargo("空度禅师")
 
 
 def df():
-    b_move_map("cac", 0, 0, log="长安 -> 驿站")
-    transfer_yz()
-    b_move_map("dtgj", 0, 0, log="大唐国境 -> 地府")
-    b_move(0, 0, log="进入地府")
-    b_move_map("df", 0, 0, log="地府 -> 地藏王府")
-    b_move(0, 0, log="进入地藏王府")
-    b_move(0, 0, log="地藏王府 -> 地藏王内府")
-    b_move(0, 0, log="进入地藏王内府")
+    b_move_map("cac", 283, 38, log="长安 -> 驿站")
+    yz_transfer()
+    while not is_place("地府"):
+        b_move_map("dtgj", 49, 332, log="大唐国境 -> 地府")
+        b_move(0, 0, log="进入地府")
+    while not is_place("地藏王府"):
+        b_move_map("df", 29, 70, log="地府 -> 地藏王府")
+        b_move(0, 0, log="进入地藏王府")
+    while not is_place("地藏王内府"):
+        b_move(0, 0, log="地藏王府 -> 地藏王内府")
+        b_move(0, 0, log="进入地藏王内府")
     release_cargo("地藏王")
 
 
 def pt():
-    b_move_map("cac", 0, 0, log="长安城 -> 大唐国境")
-    b_move(0, 0, log="进入大唐国境")
-    b_move_map("dtgj", 0, 0, log="大唐国境 -> 普陀山")
-    b_move(0, 0, log="进入普陀山")
-    b_move_map("pts", 0, 0, log="普陀山 -> 潮音洞")
-    b_move(0, 0, log="进入潮音洞")
+    while not is_place("大唐国境"):
+        b_move_map("cac", 10, 3, log="长安城 -> 大唐国境")
+        b_move(0, 0, log="进入大唐国境")
+    while not is_place("普陀山"):
+        b_move_map("dtgj", 219, 69, log="大唐国境 -> 普陀山")
+        b_move(0, 0, log="进入普陀山")
+    while not is_place("潮音洞"):
+        b_move_map("pts", 6, 65, log="普陀山 -> 潮音洞")
+        b_move(0, 0, log="进入潮音洞")
     b_move(0, 0, log="走向观音姐姐")
     info("寻找观音姐姐")
     release_cargo("观音姐姐")
@@ -241,30 +252,38 @@ def pt():
 
 def lg():
     ca_to_dhw()
-    b_move_map("dhw", 0, 0, log="东海湾 -> 龙宫")
-    b_move(0, 0, log="进入龙宫")
-    b_move_map("lg", 0, 0, log="龙宫 -> 龙宫殿")
-    b_move(0, 0, log="进入龙宫殿")
+    while not is_place("龙宫"):
+        b_move_map("dhw", 113, 90, log="东海湾 -> 龙宫")
+        b_move(0, 0, log="进入龙宫")
+    while not is_place("龙宫殿"):
+        b_move_map("lg", 113, 64, log="龙宫 -> 龙宫殿")
+        b_move(0, 0, log="进入龙宫殿")
     b_move(0, 0, log="走向东海龙王")
     release_cargo("东海龙王")
 
 
 def ne():
     ca_to_dhw()
-    b_move_map("dhw", 0, 0, log="东海湾 -> 傲来国")
-    b_move(0, 0, log="进入傲来国")
-    b_move_map("alg", 0, 0, log="傲来国 -> 女儿村")
-    b_move(0, 0, log="进入女儿村")
-    b_move_map("nec", 0, 0, log="女儿村 -> 女儿村房")
-    b_move(0, 0, log="进入女儿村房")
+    while not is_place("傲来国"):
+        b_move_map("dhw", 63, 18, log="东海湾 -> 傲来国")
+        b_move(0, 0, log="进入傲来国")
+    while not is_place("女儿村"):
+        b_move_map("alg", 6, 141, log="傲来国 -> 女儿村")
+        b_move(0, 0, log="进入女儿村")
+    while not is_place("女儿村房"):
+        b_move_map("nec", 17, 124, log="女儿村 -> 女儿村房")
+        b_move(0, 0, log="进入女儿村房")
     release_cargo("孙婆婆")
 
 
 def wz():
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 五庄观")
-    b_move_map("wzg", 0, 0, log="五庄观 -> 五庄道馆")
-    b_move(495, 283, log="进入五庄观")
+    while not is_place("五庄观"):
+        b_move_map("dtjw", 633, 85, log="大唐境外 -> 五庄观")
+        b_move(0, 0, log="进入五庄观")
+    while not is_place("五庄道馆"):
+        b_move_map("wzg", 58, 38, log="五庄观 -> 五庄道馆")
+        b_move(495, 283, log="进入五庄道馆")
     release_cargo("镇元大仙")
 
 
@@ -274,10 +293,12 @@ def ps(flag):
     flag = 2 花十娘
     """
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 盘丝岭")
-    b_move(370, 79, log="进入盘丝岭")
-    b_move_map("psl", 0, 0, log="盘丝岭 -> 盘丝洞")
-    b_move(691, 262, log="进入盘丝洞")
+    while not is_place("盘丝岭"):
+        b_move_map("dtjw", 529, 118, log="大唐境外 -> 盘丝岭")
+        b_move(370, 79, log="进入盘丝岭")
+    while not is_place("盘丝洞"):
+        b_move_map("psl", 190, 129, log="盘丝岭 -> 盘丝洞")
+        b_move(691, 262, log="进入盘丝洞")
     if flag == 1:
         b_move(359, 106)
         time.sleep(5)
@@ -292,10 +313,12 @@ def ps(flag):
 
 def mw():
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 魔王寨")
-    b_move(0, 0, log="进入魔王寨")
-    b_move_map("mwz", 0, 0, log="魔王寨 -> 魔王寨殿")
-    b_move(0, 0, log="进入魔王寨殿")
+    while not is_place("魔王寨"):
+        b_move_map("dtjw", 57, 118, log="大唐境外 -> 魔王寨")
+        b_move(0, 0, log="进入魔王寨")
+    while not is_place("魔王寨殿"):
+        b_move_map("mwz", 93, 73, log="魔王寨 -> 魔王寨殿")
+        b_move(0, 0, log="进入魔王寨殿")
     release_cargo("牛魔王")
 
 
@@ -306,21 +329,25 @@ def st(flag):
     flag = 3 三大王
     """
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 狮驼岭")
-    b_move(52, 392, log="进入狮驼岭")
+    while not is_place("狮驼岭"):
+        b_move_map("dtjw", 4, 53, log="大唐境外 -> 狮驼岭")
+        b_move(52, 392, log="进入狮驼岭")
     if flag == 1:
-        b_move_map("stl", 0, 0, log="狮驼岭 -> 大大王洞")
-        b_move(567, 255, log="进入大大王洞")
+        while not is_place("大大王洞"):
+            b_move_map("stl", 119, 29, log="狮驼岭 -> 大大王洞")
+            b_move(567, 255, log="进入大大王洞")
         b_move(527, 271, log="走向大大王")
         time.sleep(3)
         release_cargo("大大王")
     if flag == 2:
-        b_move_map("stl", 0, 0, log="狮驼岭 -> 二大王洞")
-        b_move(423, 239, log="进入二大王洞")
+        while not is_place("二大王洞"):
+            b_move_map("stl", 27, 87, log="狮驼岭 -> 二大王洞")
+            b_move(423, 239, log="进入二大王洞")
         release_cargo("二大王")
     if flag == 3:
-        b_move_map("stl", 0, 0, log="狮驼岭 -> 三大王洞")
-        b_move(345, 300, log="进入三大王洞")
+        while not is_place("三大王洞"):
+            b_move_map("stl", 15, 40, log="狮驼岭 -> 三大王洞")
+            b_move(345, 300, log="进入三大王洞")
         b_move(618, 349, log="走向三大王")
         time.sleep(5)
         release_cargo("三大王")
@@ -332,12 +359,15 @@ def tg(flag):
     flag = 2 杨戬
     """
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 长寿郊外")
-    b_move(0, 0, log="进入长寿郊外")
-    b_move_map("csjw", 0, 0, log="长寿郊外 -> 天宫")
-    b_move(0, 0, log="进入天宫")
-    b_move_map("tg", 0, 0, log="天宫 -> 凌霄宝殿")
-    b_move(0, 0, log="进入凌霄宝殿")
+    while not is_place("长寿郊外"):
+        b_move_map("dtjw", 50, 23, log="大唐境外 -> 长寿郊外")
+        b_move(0, 0, log="进入长寿郊外")
+    while not is_place("天宫"):
+        b_move_map("csjw", 22, 64, log="长寿郊外 -> 天宫")
+        b_move(0, 0, log="进入天宫")
+    while not is_place("凌霄宝殿"):
+        b_move_map("tg", 149, 62, log="天宫 -> 凌霄宝殿")
+        b_move(0, 0, log="进入凌霄宝殿")
     if flag == 1:
         b_move(0, 0, log="寻找李靖")
         release_cargo("李靖")
@@ -348,14 +378,18 @@ def tg(flag):
 
 def fc():
     ca_to_dtjw()
-    b_move_map("dtjw", 0, 0, log="大唐境外 -> 长寿郊外")
-    b_move(0, 0, log="进入长寿郊外")
-    b_move_map("csjw", 0, 0, log="长寿郊外 -> 长寿村")
-    b_move(0, 0, log="进入长寿村")
-    b_move_map("csc", 0, 0, log="长寿村 -> 方寸山")
-    b_move(0, 0, log="进入方寸山")
-    b_move_map("fcs", 0, 0, log="方寸山 -> 方寸山殿")
-    b_move(0, 0, log="进入方寸山殿")
+    while not is_place("长寿郊外"):
+        b_move_map("dtjw", 50, 23, log="大唐境外 -> 长寿郊外")
+        b_move(0, 0, log="进入长寿郊外")
+    while not is_place("长寿村"):
+        b_move_map("csjw", 160, 166, log="长寿郊外 -> 长寿村")
+        b_move(0, 0, log="进入长寿村")
+    while not is_place("方寸山"):
+        b_move_map("csc", 110, 207, log="长寿村 -> 方寸山")
+        b_move(0, 0, log="进入方寸山")
+    while not is_place("方寸山殿"):
+        b_move_map("fcs", 131, 137, log="方寸山 -> 方寸山殿")
+        b_move(0, 0, log="进入方寸山殿")
     b_move(0, 0, log="走向菩提祖师")
     release_cargo("菩提祖师")
 
---
+++ main/test.py
@@ -0,0 +1,5 @@
+from main.biao import *
+
+if __name__ == '__main__':
+    load_driver()
+    print("测试---")
